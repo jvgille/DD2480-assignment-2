612adaf9bddd83dd99a4fdff6baa5b25b1107c4e
joel ekelöf <joeleke@kth.se>
2019-02-07 15:46:13 +0100

    added again

---
+++ src/main/java/group22/PushPayload.java
@@ -0,0 +1,27 @@
+package group22;
+
+public class PushPayload {
+    final String ref;
+    final String pusherName;
+    final String pusherMail;
+    final String commitSHA;
+    final String repoURL;
+
+    String buildResult;
+    String buildMessage;
+    String date;
+
+    public PushPayload(String ref, String pusherName, String pusherMail, String commitSHA, String repoURL, String date) {
+        this.ref = ref;
+        this.pusherName = pusherName;
+        this.pusherMail = pusherMail;
+        this.commitSHA = commitSHA;
+        this.repoURL = repoURL;
+        this.date = date;
+    }
+
+    @Override
+    public String toString() {
+        return "ref: " + ref + "\nURL: " + "\nemail: " + pusherMail + "\nname: " + pusherName + "\nSHA: " + commitSHA + "\ndate: " + date;
+    }
+}
