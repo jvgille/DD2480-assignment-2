f0c82124e35b997e411c43e29da8e30a91297547
Arthur <acvignon@kth.se>
2019-02-08 09:51:46 +0100

    #30 Add comment to PushPayload class

--- src/main/java/group22/PushPayload.java
+++ src/main/java/group22/PushPayload.java
@@ -1,6 +1,6 @@
 package group22;
-/*
-* A utility class that stores important information about a commit.
+/**
+* A utility class that stores all the needed information about the commit after parsing the http payload.
 */
 public class PushPayload {
     final String ref;
@@ -13,6 +13,15 @@ public class PushPayload {
     String buildMessage;
     String date;
 
+    /**
+     * Create a PushPayload object with the given parameters.
+     * @param ref: the reference to the branch which was modified
+     * @param pusherName: the name of the user who committed the changed
+     * @param pusherMail: the email to which the server will send the approval
+     * @param commitSHA: the SHA associated to the commit
+     * @param repoURL: the URL for the repository
+     * @param date: The date of the commit
+     */
     public PushPayload(String ref, String pusherName, String pusherMail, String commitSHA, String repoURL, String date) {
         this.ref = ref;
         this.pusherName = pusherName;
