e5c3eaf94fa701be72bf9b6a50fa8e6c4cdb9131
Paul <griesser@kth.se>
2019-02-08 10:57:17 +0100

    #30 Update comment to the HistoryLogger class

--- src/main/java/group22/HistoryLogger.java
+++ src/main/java/group22/HistoryLogger.java
@@ -6,15 +6,22 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * A utility class that enables to store persistently the histoy of the builds
+ */
 public class HistoryLogger {
 
+    /**
+     * Create the directories where the logs of the builds will be stored
+     */
     private static void makeDir() {
         File f = new File(ContinuousIntegrationServer.BUILDS_PATH);
         f.mkdirs();
     }
 
     /**
-     * Updates the data/index.html file to include a link to the given build.
+     * Update the data/index.html file to include a link to the given build.
+     * @param p the PushPayload that contains the SHA of the commit
      */
     private static void updateIndex(PushPayload p) throws IOException {
         BufferedWriter w = new BufferedWriter(new FileWriter(ContinuousIntegrationServer.INDEX_PATH, true));
@@ -24,6 +31,7 @@ public class HistoryLogger {
 
     /**
      * Stores the result of a given build in a formatted html file inside data/builds/
+     * @param p The PushPayload that contains the information about the commit
      */
     public static void storeBuild(PushPayload p) throws IOException {
         makeDir();
@@ -33,7 +41,7 @@ public class HistoryLogger {
         w.println("<p><strong><a href=\"/\">Back</a></strong></p>");
         w.println("<p><strong>SHA:</strong> " + p.commitSHA +  "</p>");
         w.println("<p><strong>Author:</strong> " + p.pusherName + "</p>");
-        w.println("<p><strong>Date:</strong> " + p.date + "</p>");   
+        w.println("<p><strong>Date:</strong> " + p.date + "</p>");
         w.println("<p><strong>Status:</strong> " + p.buildResult + "</p>");
         w.println("<p><strong>Logs:</strong></p> ");
         w.println("<div style=\"border: 1px solid black; background:#ffffcc\"><pre>");
