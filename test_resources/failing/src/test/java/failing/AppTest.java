5f035627a5ad36ad5b795936177493ca9f92e545
Joel <jvgille@users.noreply.github.com>
2019-02-09 17:39:49 +0100

    #31 add testing projects

---
+++ test_resources/failing/src/test/java/failing/AppTest.java
@@ -0,0 +1,11 @@
+package failing;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+    @Test public void testAppHasAGreeting() {
+        // This a testing project which is not going to build.
+        assertTrue(false);
+    }
+}
