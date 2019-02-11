fa711cf4c0fc66c546a9ca8c0fa9fd0d21e2c929
Paul <griesser@kth.se>
2019-02-11 17:33:17 +0100

    #31 add test for the mail notification

---
+++ src/test/java/group22/MailProcessTest.java
@@ -0,0 +1,19 @@
+package group22;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Class for testing the mail system
+ */
+public class MailProcessTest {
+    /**
+     * Test that our notification system works properly and that the subprocess doesn't return an error
+     */
+    @Test public void test1() throws Exception {
+        PushPayload p = new PushPayload("refs/heads/myBranchh", "Paul", "griesser@kth.se", "af73722fa8737de3b33b4069d509156cbceb822b",  "https://github.com/GriesserP/hello-test", "2019-02-11T16:24:10+01:00");
+        ProcessBuilder b = new ProcessBuilder();
+        b.command("bash", "-c", "mail -s \"Your latest push\" "+ p.pusherMail +" <<< '"+ p.buildResult + "'");
+        assertTrue(b.start().waitFor() == 0);
+    }
+}
